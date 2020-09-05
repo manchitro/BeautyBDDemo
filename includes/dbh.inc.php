9d8b6f04b33e1ddb6807208ea75412d0351f24ed
AsefHossainKhan <56684655+AsefHossainKhan@users.noreply.github.com>
2020-09-05 13:34:30 +0600

    included new server name

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,14 @@
 <?php
-$servername = "cloudmysql.gotdns.ch";
-$dbusername = "asef";
-$dbpassword = "qZ{jDZSxycn+&y56";
-$dbname = "polapain";
-// $servername = "localhost";
-// $dbusername = "root";
-// $dbpassword = "";
-// $dbname = "testfb";
+// $servername = "cloudmysql.gotdns.ch";
+// cloudmysql.duckdns.org
+
+// $dbusername = "asef";
+// $dbpassword = "qZ{jDZSxycn+&y56";
+// $dbname = "polapain";
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "testfb";
 
 $conn = mysqli_connect($servername,$dbusername,$dbpassword,$dbname);
 
